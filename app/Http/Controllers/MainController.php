23082397ebf43918b92d1b15e95b5a4e684b8ba9
Gian Michel <gianmnf@gmail.com>
2025-08-29 18:05:46 -0300

    002

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,7 +9,34 @@ class MainController extends Controller
 {
     public function index()
     {
-        DB::connection()->getPdo();
-        echo 'OK!';
+        $clients = DB::table('clients')->get(); // Select * from clients
+        // $this->showRawData($clients);
+        $this->showDataTable($clients);
+    }
+
+    private function showRawData($data)
+    {
+        echo '<pre>';
+        print_r($data);
+        echo '</pre>';
+    }
+
+    private function showDataTable($data)
+    {
+        echo '<table border="1">';
+        // header
+        echo '<tr>';
+        foreach($data[0] as $key=>$value){
+            echo '<th>' . $key . '</th>';
+        }
+        echo '</tr>';
+        foreach ($data as $row) {
+            echo '<tr>';
+            foreach ($row as $key => $value) {
+                echo '<td>' . $value . '</td>';
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
     }
 }
