ed6db482e01b58b693c734d57305d71fb165fef7
Gian Michel <gianmnf@gmail.com>
2025-09-01 12:49:15 -0300

    003

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -9,9 +9,46 @@ class MainController extends Controller
 {
     public function index()
     {
-        $clients = DB::table('clients')->get(); // Select * from clients
-        // $this->showRawData($clients);
-        $this->showDataTable($clients);
+        // returning all data from a table
+        // $clients = DB::table('clients')->get();
+
+        // show in a array
+        // $clients = DB::table('clients')->get()->toArray();
+
+        // show in a array of arrays
+        // $results = DB::table('products')->get()->map(function($item){
+        //     return (array) $item;
+        // });
+
+        // show data from the results
+        // $products = DB::table('products')->get();
+        // foreach ($products as $product) {
+        //     echo $product->product_name . '<br>';
+        // }
+
+        // get only a few columns
+        // $products = DB::table('products')->get(['product_name', 'price']);
+        // $this->showDataTable($products);
+
+        // pluck - get data from a column in a simple way
+        // $results = DB::table('products')->pluck('product_name');
+        // $this->showRawData($results);
+
+        // return only the first line of a result
+        // $results = DB::table('products')->get()->first();
+        // $this->showRawData($results);
+
+        // return only the last line of result
+        // $results = DB::table('products')->get()->last();
+        // $this->showRawData($results);
+
+        // specific id
+        $results = DB::table('products')->find(10);
+        $this->showRawData($results);
+
+
+        //$this->showRawData($products);
+        // $this->showDataTable($clients);
     }
 
     private function showRawData($data)
